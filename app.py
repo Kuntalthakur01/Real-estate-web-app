23da5369ab625424a4964864e4b01553ede160ca
Vaishnavi Sarmalkar <99071024+Vaishnavi0805@users.noreply.github.com>
2023-01-03 14:59:52 +0530

    app

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 
 app = Flask(__name__,template_folder='templates')
-
+# global area
+# area=None
 @app.route("/",methods=['GET','POST'])
 def price_predictor():
+    area=None
     if request.method == 'POST':
         area=request.form['area']
         bhk=request.form['bhk']
-        return area+bhk
-    return render_template('predict.html')
+        print(area+bhk)
+        
+        # return redirect(url_for('op'))
+    return render_template('predict.html',area=area)
+     
+
+# @app.route("/op",methods=['GET','POST'])
+# def op():
+#     if request.method == 'POST':
+
+#     # a=area
+#         print(f"area: {area}")
+#         return redirect(url_for('price_predictor'))
+#     return render_template("predict.html",area=area)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
