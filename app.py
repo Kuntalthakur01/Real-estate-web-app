0b2046bc583609620c898ceb3f7502b3c9c671aa
Vaishnavi Sarmalkar <99071024+Vaishnavi0805@users.noreply.github.com>
2023-01-04 09:49:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
 
-
 app = Flask(__name__,template_folder='templates')
-
-#  SQL CREDENTIALS
-     
-
-## SQL INTEGRATION
-
-
-## PREDICTION FUNCTION
+# global area
+# area=None
 @app.route("/",methods=['GET','POST'])
 def price_predictor():
     area=None
@@ -18,8 +11,19 @@ def price_predictor():
         bhk=request.form['bhk']
         print(area+bhk)
         
-        # return redirect(url_for('op'))
     return render_template('predict.html',area=area)
+
+
+# @app.route("/op",methods=['GET','POST'])
+# def op():
+#     if request.method == 'POST':
+
+#     # a=area
+#         print(f"area: {area}")
+#         return redirect(url_for('price_predictor'))
+#     return render_template("predict.html",area=area)
+
+
 
 
 if __name__ == "__main__":
