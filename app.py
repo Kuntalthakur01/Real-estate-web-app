8f89ac171428a030b96ca7a818dbd89a7e47543f
Vaishnavi Sarmalkar <99071024+Vaishnavi0805@users.noreply.github.com>
2023-01-04 10:51:26 +0530

    app.py model integrated

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 from flask import Flask, request, render_template, redirect, url_for
-
+import numpy as np
+import pickle
 app = Flask(__name__,template_folder='templates')
-# global area
-# area=None
+model=pickle.load(open("model.pkl","rb"))
+#  SQL CREDENTIALS
+     
+
+## SQL INTEGRATION
+
+
+## PREDICTION FUNCTION
 @app.route("/",methods=['GET','POST'])
 def price_predictor():
-    area=None
     if request.method == 'POST':
-        area=request.form['area']
-        bhk=request.form['bhk']
-        print(area+bhk)
-        
-    return render_template('predict.html',area=area)
-
-
-# @app.route("/op",methods=['GET','POST'])
-# def op():
-#     if request.method == 'POST':
-
-#     # a=area
-#         print(f"area: {area}")
-#         return redirect(url_for('price_predictor'))
-#     return render_template("predict.html",area=area)
-
-
-
+        int_features=[int(x) for x in request.form.values()]
+        final_features=[np.array(int_features)]
+        price=model.predict(final_features)[0][0]
+        final_price=np.round(price,2)
+        print(f"Final features:{final_features}")
+        return render_template('predict.html',final_price=final_price)
+    return render_template('predict.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
